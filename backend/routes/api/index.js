ca0d3c8b2cc7696f11556d5e5c86a08c3d10a4b9
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-01-22 12:11:57 -0700

    user uath, phase 3 complte

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body })
 });
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         },
+//     })
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 
 module.exports = router;
