8523afb2034064f51e88a38a036385e03643341d
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-01-22 13:41:50 -0700

    logout route complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,4 +29,12 @@ router.post(
     }),
 );
 
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
 module.exports = router;
