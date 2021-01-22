9aa94eec8e0bee766d2b454ea8310effabf85593
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-01-22 13:34:07 -0700

    login route finished

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body })
