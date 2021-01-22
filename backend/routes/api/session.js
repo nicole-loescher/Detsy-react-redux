dcc8f98e8cf342020566b064ecd4409e5e4fb672
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-01-22 14:03:38 -0700

    get user session complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -37,4 +37,17 @@ router.delete(
     }
 );
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
+
 module.exports = router;
